02458e9371277efdbae2cd00f86f125906fc4689
Gautam <>
2019-09-27 12:35:17 -0400

    Shoould work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME="/usr/local/openjdk-8/bin/java"
+export JAVA_HOME="/usr/local/openjdk-8"
 bash <(curl -s https://detect.synopsys.com/detect.sh) \
 --blackduck.api.token="$BLACKDUCK_API_TOKEN" \
 --blackduck.url="$BLACKDUCK_URL" \
